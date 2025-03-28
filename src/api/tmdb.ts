569b7144b5bacc634c8c63d2452a736d687f8fca
Your Name <you@example.com>
2025-03-29 04:33:37 +0800

    3

--- src/api/tmdb.ts
+++ src/api/tmdb.ts
@@ -1,15 +1,16 @@
 // api/tmdb.ts
-import axios from 'axios' // 引入axios
-import { ElMessage, ElMessageBox } from 'element-plus'
-import { useUserStore } from '@/pinia/modules/user'
-import router from '@/router/index'
+// api/axios.ts
+import axios from 'axios'
 
+const tmdbApi = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+  headers: {
+    Authorization: `Bearer ${import.meta.env.VITE_TMDB_ACCESS_TOKEN}`,
+  },
+})
+
+export const getDiscover = (params: PaginationParams) => 
+  tmdbApi.get('/discover/movie', { params })
 export const getVideos = (params: PaginationParams) => 
   axios.get('/videos', { params })
 
-export const getDiscover = (params: PaginationParams) => 
-  axios.get('https://api.themoviedb.org/3/discover/movie', params, {
-    headers: {
-      "Authorization": "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiM2EyYTU0NWVmNjAwNTVjNjU0OTUyNGExNmU2OGRhNSIsIm5iZiI6MTczOTM3MjA0My45NzgsInN1YiI6IjY3YWNiNjBiZTA5ZDZjOTE5MGIwYTRhMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.O97KTBFbCV8rCwAJgmW6hNe6moQZqGFf3Y69uULCeEI",
-    }
-  })
